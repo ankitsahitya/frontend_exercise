bbf1dcc3931a0f46b72fad326633302ffb72984c
ankitsahitya <ankit@gkmit.co>
2018-01-27 21:51:24 +0530

    javascript partialy completed

---
+++ assets/js/index.js
@@ -0,0 +1,90 @@
+var count = 0;
+var index = 0;
+var timer;
+carousel();
+function carousel() {
+	clearTimeout(timer);
+  var i;
+  $("#slide1").attr("style","display:none");
+  $("#slide2").attr("style","display:none");
+  if(index%2 == 0){
+  	$("#slide1").attr("style","display:block");
+  } 
+  else{
+  	$("#slide2").attr("style","display:block");
+  }
+  index++;
+  timer = setTimeout(carousel, 8000);
+}
+$(document).ready(function(){
+	$("#home").attr("style", "display:block");
+	$("#example").attr("style", "display:none");
+	$("#feedback").attr("style", "display:none");
+	$("#image-effect").attr("style", "display:none");
+	$("#contact-us").attr("style", "display:none");
+});
+function openNav() {
+  var x = document.getElementById("topnav");
+  if (x.className == "topnav") {
+
+  console.log("a");
+    x.className += " responsive";
+  } else {
+  console.log("!a");
+    x.className = "topnav";
+  }
+}
+function openBlock(blockName){
+	if(blockName == "home" || blockName == "example"){
+		$("#right-block").attr("style", "display:block");
+	}
+	else{
+		$("#right-block").attr("style", "display:none");
+	}
+	$("#home-link").removeClass("active");
+	$("#example-link").removeClass("active");
+	$("#feedback-link").removeClass("active");
+	$("#image-effect-link").removeClass("active");
+	$("#contact-us-link").removeClass("active");
+	$("#"+blockName+"-link").addClass("active");
+	$("#home").attr("style", "display:none");
+	$("#example").attr("style", "display:none");
+	$("#feedback").attr("style", "display:none");
+	$("#image-effect").attr("style", "display:none");
+	$("#contact-us").attr("style", "display:none");
+	$("#"+blockName).attr("style", "display:block");
+}
+function glow(){
+	if(count%2 == 0){
+		$("#switch").attr("style","background-color:red");
+		$("#bulb").attr("src","assets/images/bulbon");
+		$("#switch").text("Switch on");
+		count++;
+	}
+	else{
+		$("#switch").attr("style","background-color:green");
+		$("#bulb").attr("src","assets/images/bulboff.png");
+		$("#switch").text("Switch off");
+		count++;
+	}
+}
+popup = function() {
+	$("#commenter").val(null);
+	$("#comment").val(null);
+  $("#modal").attr("style","display:block;");
+}
+closeModel = function() {
+  $("#modal").attr("style","display:none;");
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    $("#modal").attr("style","display:none;");
+  }
+}
+var i=1;
+function addComment(commenter,comment){
+	var row = '<tr><td> ' + i + ' </td> <td> ' + commenter + ' </td> <td>' + comment + '</td></tr>';
+  $("#comments").append(row);
+  i++;
+  closeModel();
+}
